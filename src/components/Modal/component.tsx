5dfe31c4c18ff08b60020c44cdd9cb3298280119
nol4 <nome.ahumada@outlook.com>
2024-07-24 13:10:30 -0400

    feat: Crear componente Modal
    - Interfaz ModalProps definida con las propiedades show, onClose y children.
    - Componente Modal que acepta props de tipo ModalProps.
    - Lógica para mostrar u ocultar el modal basada en la propiedad show.
    - Implementación de un overlay que cubre la pantalla y cierra el modal al hacer clic.
    - Contenedor principal del modal con diseño centrado, con botón de cierre y espacio para contenido (children).
    - Estilos aplicados para mejorar la apariencia del modal.

---
+++ src/components/Modal/component.tsx
@@ -0,0 +1,49 @@
+
+interface ModalProps {
+    show: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+/**
+ * Componente Modal.
+ * 
+ * @param {Object} props - Props para el componente Modal.
+ * @param {boolean} props.show - Indica si el modal se muestra o no.
+ * @param {() => void} props.onClose - Función que se llama al cerrar el modal.
+ * @param {React.ReactNode} props.children - Contenido del modal.
+ * @returns Elemento TSX del modal o null si no se debe mostrar.
+ */
+const Modal = ({ show, onClose, children }: ModalProps) => {
+
+    if (!show) return null;
+
+    return (
+        <>
+            <div className="fixed inset-0 z-[61] h-full w-full flex items-center justify-center">
+
+                {/* Overlay */}
+                <div className="fixed z-[60] top-0 left-0 w-full h-full bg-black opacity-50 flex items-center justify-center">
+                    <div className="absolute top-0 left-0 w-full h-full" onClick={onClose}></div>
+                </div>
+                {/* End Overlay */}
+
+                <div className="rounded-lg shadow-lg z-[62] max-w-lg w-full max-h-95vh overflow-y-auto">
+
+                    <div className="relative bg-white p-6">
+                        <button
+                            className="absolute text-4xl top-0 right-2 hover:text-gray-900 hover:text-blue-900 hover:font-extrabold"
+                            onClick={onClose}
+                        >
+                            &times;
+                        </button>
+                        {children}
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Modal;
